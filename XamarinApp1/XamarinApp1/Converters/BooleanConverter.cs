4643ff5b28f72b18c86cc83381eb55a0ba8237a2
Utsav <utsavdj@gmail.com>
2018-12-04 12:31:44 +0545

    Signed-off-by: Utsav <utsavdj@gmail.com>

---
+++ XamarinApp1/XamarinApp1/Converters/BooleanConverter.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinApp1.Converters
+{
+    public class BooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return false;
+            return !(value as Boolean?);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value ;
+        }
+    }
+}
